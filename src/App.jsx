d533886a958c1f13db96cf5be285fbda7b7790fa
Vũ Đức Tâm <ductamyenphucnamdinh@gmail.com>
2024-06-18 18:56:13 +0700

    dark-light

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,27 @@ import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import HomeIcon from '@mui/icons-material/Home'
-
 import { pink } from '@mui/material/colors'
-import  Typography  from '@mui/material/Typography'
+import Typography from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
 
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 function App() {
 
   return (
     <>
+      <ModeToggle />
       <div>hoanglananh</div>
       <Typography variant='' color='text.secondary'>hoanglananh</Typography>
       <Button variant="text">Text</Button>
@@ -29,5 +42,4 @@ function App() {
     </>
   )
 }
-
 export default App
